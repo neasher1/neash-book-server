27a77b67d810df616450cd51a6376d3d0f9025db
neasher <neasher75@gmail.com>
2022-12-29 01:30:22 +0600

    JWT and users save in db

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
+//JWT verify
+const verifyUser = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    };
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            res.status(403).send({ message: '403 Forbidden' })
+        }
+        req.decoded = decoded;
+    });
+    next();
+
+}
+
 //MongoDb Credentials Setup
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hlzaati.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,6 +39,20 @@ const run = async () => {
     const usersCollection = client.db('neashBook').collection("users");
 
     try {
+
+        //jwt user token
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '20d' });
+            res.send({ accessToken: token });
+        });
+
+        //save users info in db
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
     }
 
